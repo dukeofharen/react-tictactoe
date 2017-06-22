f7ec03e1cd6526d9b075fdadb7bbd40fd0df1032
Duco Winterwerp <duco@winterwerp.nl>
2017-06-22 21:09:47 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Game from './Game';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+ReactDOM.render(<Game />, document.getElementById('root'));
+registerServiceWorker();
